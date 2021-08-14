927eaa8368aab5bbe29d18dbc2557fc39b151f4a
marty808 <helge@brust-mail.de>
2021-08-14 12:46:30 +0200

    initial commit

---
+++ install-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1. enable IPv6 for docker
+# get ip addr
+INTERFACE="eth0"
+IPV6=$(netplan get ethernets.${INTERFACE}.addresses | cut -d' ' -f2)
+
+# config deamon.json
+echo '{
+  "ipv6": true,
+  "fixed-cidr-v6": "'${IPV6}'"
+}' > /etc/docker/daemon.json
+
+# reload docker
+systemctl restart docker
+
+# 2. install docker-compose
+apt-get update  && apt-get install -y docker-compose
